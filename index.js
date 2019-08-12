c64e59dc8df87a708493f3530bb2dccd8e2657fc
Adeshkyal31 <adesh.kyal@example.com>
2019-08-12 22:08:35 +0530

    update receiver's domain

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8081;
 
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://digittweet-client.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://digittweet-client.herokuapp.com"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, application/json, Accept");
   next();
 })
